acce7954e8fe228d078ba54fb930aab560bdd5f7
kairi003 <kairi.satellite@gmail.com>
2021-04-22 01:56:54 +0900

    first

---
+++ fgo-material-simulator/userscript/fxmat.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         fxmat
+// @namespace    https://github.com/kairi003/
+// @version      1.0
+// @description  Fix FGO Material Simulater
+// @author       kairi_003
+// @match        http://fgosimulator.webcrow.jp/Material/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.head.insertAdjacentHTML('beforeend','<style>.icon{max-height:73px !important;}.box-chaldea{margin:2px !important;}.white{box-sizing:border-box !important;}</style>');
+})();
